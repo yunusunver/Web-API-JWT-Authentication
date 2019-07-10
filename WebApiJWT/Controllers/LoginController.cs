59f960bee34d91b7b0590c35cf442719b8ce92c2
yunver <yunus.unver1995@gmail.com>
2019-07-10 18:18:35 +0300

    Added Login Controller

---
+++ WebApiJWT/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApiJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserModel login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if ( user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new {token = tokenString});
+            }
+
+            return response;
+        }
+
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials= new SigningCredentials(securityKey,SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                null,
+                expires:DateTime.Now.AddMinutes(120),
+                signingCredentials:credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+
+            if (login.Username == "kullanici")
+            {
+                user = new UserModel{Username="kullanici",EmailAdress="test@gmail.com"};
+            }
+
+            return user;
+        }
+    }
+}
